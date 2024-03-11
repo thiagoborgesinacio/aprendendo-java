891fabff2234a7ae8bdb6226a8a27cd60681812f
Thiago Borges Inácio <159143900+thiagoborgesinacio@users.noreply.github.com>
2024-03-11 11:49:23 -0300

    finalizado ex HelloWorld1

--- HelloWorld1.java
+++ HelloWorld1.java
@@ -1,6 +1,14 @@
 public class HelloWorld1 {
     
     public static void main(String[] args) {
-         System.out.println("Hello World 1");
+         String ambiente = args[0];
+         if (ambiente.equals("DEV")) {
+            System.out.print("Executando em DEV");
+         } else if (ambiente.equals("TESTE")) {
+            System.out.print("Executando em TESTE");
+         } else {
+            System.out.print("ERROR!");
+         }
+         
     }
 }
